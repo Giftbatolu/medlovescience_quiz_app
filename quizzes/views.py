1a7cc7925c9ac92d6f25833c710c5dbc87e2e154
giftbatolu <sekinatoyelami@gmail.com>
2026-01-06 23:54:31 +0100

    docs: add comment to view for the quiz

--- quizzes/views.py
+++ quizzes/views.py
@@ -4,6 +4,16 @@ from .models import Quiz, Question, Option
 from rest_framework import viewsets
 
 class QuizViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing quizzes.
+    Provides CRUD operations for Quiz model.
+    
+    get_serializer_class:
+        Dynamically selects the serializer based on the action:
+        - list: Uses QuizListSerializer to list all quizzes.
+        - retrieve: Uses QuizDetailSerializer to provide detailed information about a specific quiz.
+        - default: Uses QuizSerializer for other actions (create, update, delete).
+    """
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
 
@@ -15,6 +25,16 @@ class QuizViewSet(viewsets.ModelViewSet):
         return super().get_serializer_class()
 
 class QuestionViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing questions within a specific quiz.
+    Provides CRUD operations for Question model filtered by quiz.
+
+    get_queryset:
+        Filters questions based on the quiz_pk provided in the URL.
+
+    perform_create:
+        Associates a new question with the specified quiz during creation.
+    """
     serializer_class = QuizQuestionSerializer
 
     def get_queryset(self):
